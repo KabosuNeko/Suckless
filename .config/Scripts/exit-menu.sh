47119970225fa472b3729afe909576cf496ac78d
KabosuNeko <235253880+KabosuNeko@users.noreply.github.com>
2026-03-14 21:19:50 +0700

    Add scripts and tweaks picom

---
+++ .config/Scripts/exit-menu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Exit Menu using Rofi
+
+OPTIONS=" Lock\n󰒲 Suspend\n Reboot\n⏻ PowerOff\n󰍃 LogOut"
+
+SELECTED=$(echo -e "$OPTIONS" | rofi -dmenu -i -p "Power Menu:" -theme ~/.config/rofi/powermenu.rasi)
+
+# Extract only the action word
+ACTION=$(echo "$SELECTED" | awk '{print $2}')
+
+case "$ACTION" in
+    Lock)
+        # Assuming you use slock, i3lock, or xscreensaver
+        slock || i3lock-color || xscreensaver-command -lock
+        ;;
+    Suspend)
+        systemctl suspend
+        ;;
+    Reboot)
+        systemctl reboot
+        ;;
+    PowerOff)
+        systemctl poweroff
+        ;;
+    LogOut)
+        # Kill dwm
+        pkill dwm
+        ;;
+esac
